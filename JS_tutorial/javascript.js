3ca428c8f2cc05b3ef6a32b23039e576746b6bd5
Jorge Moreno <jmoreno@gatech.edu>
2025-08-23 08:34:02 -0600

    Add JS files for tutorial

---
+++ JS_tutorial/javascript.js
@@ -0,0 +1,18 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+
+console.log(age); // what will be output now?
+
+
+const pi = 3.14;
+pi = 10;
+
+console.log(pi); // What will be output?
